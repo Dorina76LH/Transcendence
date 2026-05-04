878e0293c1380fe57a7c6cfdcbda335f9eee3770
jvega <jvega@student.42.fr>
2026-05-04 17:28:21 +0200

    avancenment de Web socket, réussi le test

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -1,5 +1,19 @@
-import json 
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+
 class ChatTestConsumer(AsyncWebsocketConsumer):
-	async def connect(self)
+		async def connect(self):
+				await self.accept()
+
+		async def disconnect(self, close_code):
+				pass
+
+		async def receive(self, text_data):
+				data = json.loads(text_data)
+				message = data.get('message', '')
+
+				await self.send(text_data=json.dumps({
+						'message': message
+				}))
