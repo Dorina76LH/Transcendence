e3ce3a3ed90ad58229ce61167b371e0f7471bc69
doberes <dorina.beres@gmail.com>
2026-05-18 13:16:01 +0200

    feat(chat): rapatriement complet de backend/apps/chat

--- backend/apps/chat/models.py
+++ backend/apps/chat/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Conversation(models.Model):
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='conversations'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Conversation {self.id}"
+    
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Message {self.id} from {self.sender}"
---
+++ backend/apps/chat/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Conversation, Message
+from django.contrib.auth import get_user_model
+
+User = get_user_model();
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['id', 'username', 'email']
+
+class MessageSerializer(serializers.ModelSerializer):
+	sender = UserSerializer(read_only=True)
+
+	class Meta:
+		model = Message
+		fields = ['id', 'conversation', 'sender', 'content', 'created_at' ]
+		read_only_fields = ['id', 'sender', 'created_at']
+
+class ConversationSerializer(serializers.ModelSerializer):
+	participants = UserSerializer(many=True, read_only = True)
+
+	class Meta:
+		model = Conversation
+		fields = ['id', 'participants','created_at']
+
+class ConversationCreateSerializer(serializers.Serializer):
+	participant_id = serializers.IntegerField()
