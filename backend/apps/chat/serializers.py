bdbcf21d33a097cb4c14bff07fab87620511d4a2
jvega <jvega@student.42.fr>
2026-05-19 14:57:37 +0200

    Estable - funcional - créer ou retrouver une conversation avec un autre utilisateur en POST
    docker - j'ai retire des container_name et j ái ajoute COMPOSE_PROJECT_NAME en .env

--- backend/apps/chat/serializers.py
+++ backend/apps/chat/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Conversation, Message
 from django.contrib.auth import get_user_model
 
-User = get_user_model();
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
