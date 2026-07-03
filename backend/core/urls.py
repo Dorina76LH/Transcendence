766e67a68774adce52679ff42eb94a9f12101a92
jvega <jvega@student.42.fr>
2026-07-03 18:03:30 +0200

    fix urlpatterns sans debug

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -64,5 +64,4 @@ urlpatterns = [
 		path('api/analytics/', include('apps.analytics.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
