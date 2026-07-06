b2f7dc3a5e9a99b2f1d4f366fa2932d469123fdd
jeffreyvega94 <jdvega029@hotmail.com>
2026-07-06 15:47:59 +0200

    check deps port

--- infra/tools/check_deps.sh
+++ infra/tools/check_deps.sh
@@ -20,12 +20,12 @@ if ! docker compose version &> /dev/null; then
     exit 1
 fi
 
-# # 3. Vérifier si les ports sont libres
-# if ss -tuln | grep -qE ':80\b|:443\b'; then
-#     echo -e "${RED}Error: Port 80 or 443 is already in use.${NC}"
-#     echo "Stop any other web server (nginx, apache) before running."
-#     exit 1
-# fi
+# 3. Vérifier si les ports sont libres
+if ss -tuln | grep -qE ':8080\b|:8443\b'; then
+    echo -e "${RED}Error: Port 8080 or 8443 is already in use.${NC}"
+    echo "Stop any other web server (nginx, apache) before running."
+    exit 1
+fi
 
 echo -e "${GREEN}All dependencies are OK! Starting the engine...${NC}"
 exit 0
